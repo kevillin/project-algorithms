a760e84ee2e1a7986662f35663fd79eed7ec9293
Kevillin Santos <kevillin_santos@hotmail.com>
2023-04-27 09:45:56 -0300

    feat: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if not target_time:
+        return None
+
+    for enter, exit in permanence_period:
+        if not enter or not exit or type(enter) == str or type(exit) == str:
+            return None
+        elif target_time >= enter and target_time <= exit:
+            counter += 1
+    return counter
